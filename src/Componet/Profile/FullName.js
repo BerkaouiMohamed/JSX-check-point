a9ef5b2aefeb6edaab719c20c6ca8a5d12b8eade
BerkaouiMohamed <berkaouimed@outlook.fr>
2022-08-26 20:54:52 +0100

    checkpoint JSX

---
+++ src/Componet/Profile/FullName.js
@@ -0,0 +1,9 @@
+
+function FullName(){
+return(
+    
+   <h1> Berkaoui Mohamed</h1>
+)
+
+}
+export default FullName;
---
+++ src/Componet/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import imge from '../../Assets/img.jpg'
+function ProfilePhoto(){
+return(
+    
+    <>
+    <img src={imge} alt=""/>
+    </>
+)
+
+}
+export default ProfilePhoto;
